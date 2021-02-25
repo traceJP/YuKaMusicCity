24fe39a4b61769c003ee2b7f54e9e85dfe9f70ab
traceJP <13717067350@163.com>
2021-02-25 15:07:19 +0800

    修改web文件命名为webapp，并将其目录移动至src/main下

--- src/test/java/com/tracejp/yuka/sqlDataTest/AddMusicResourcesTable.java
+++ src/test/java/com/tracejp/yuka/sqlDataTest/AddMusicResourcesTable.java
@@ -14,21 +14,21 @@ import java.util.List;
 public class AddMusicResourcesTable {
 
     // 本地音乐资源url
-    private final String localMusicResourcesURL = "../../sqlresources/musicAudio/";
+    private final String localMusicResourcesURL = "/usr/local/tomcat/webapps/YuKaMusicCity/main/fileSQL/audio/";
     // 本地音乐图片url
     private final String localMusicImgURL = "../../sqlresources/musicImg/";
 
     @Test
     public void start() throws SQLException {
         Connection conn = JdbcUtil.getConnection();
-        String sql = "INSERT INTO `music_resources`(gmt_create, gmt_modified, music_name, music_img_url, music_url, music_author) " +
-                "VALUES(NOW(),NOW(),?,?,?,?) ";
+        String sql = "INSERT INTO `music_resources`(music_name, music_img_url, music_url, music_author) " +
+                "VALUES(?,?,?,?) ";
         String[][] result = getFileName();
         for(int i = 0;i < result.length;i++) {
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, result[i][1]);
             // 图片
-            ps.setString(2, "../image/ge3.jpg");
+            ps.setString(2, null);
             ps.setString(3, result[i][2]);
             ps.setString(4, result[i][0]);
             ps.execute();
@@ -37,7 +37,7 @@ public class AddMusicResourcesTable {
     }
 
     private String[][] getFileName() {
-        File file = new File("F:\\muse\\");
+        File file = new File("D:\\CloudMusic\\");
         String[] fileList = file.list();
         // 结果返回集
         String rts[][] = new String[fileList.length][3];
